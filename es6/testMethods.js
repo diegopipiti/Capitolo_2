ca6fa6796985d27a65c2a79ce46197ff8a92eb92
marcelloburattini79 <burattinimarcello@yahoo.it>
2020-03-09 18:07:52 +0100

    Prove 9 marzo

---
+++ es6/testMethods.js
@@ -0,0 +1,36 @@
+function mescola(denari = 
+    [
+        "asso", 
+        "due", 
+        "tre", 
+        "quattro", 
+        "cinque", 
+        "sei", 
+        "sette", 
+        "fante", 
+        "cavallo", 
+        "re"
+    ])
+
+    let denariMesc = [];
+
+    
+
+
+{
+
+}
+
+let denari = 
+[
+    "asso", 
+    "due", 
+    "tre", 
+    "quattro", 
+    "cinque", 
+    "sei", 
+    "sette", 
+    "fante", 
+    "cavallo", 
+    "re"
+];
