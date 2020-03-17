f1e8886de01946ed851155c9f92f44c7a5e9778a
marcelloburattini79 <burattinimarcello@yahoo.it>
2020-03-17 09:50:30 +0100

    si possono inserire valore!!

---
+++ es6/sette_e_mezzo.js
@@ -0,0 +1,203 @@
+'use strict'
+
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let nomi = 
+[
+    'Asso',
+    'Due',
+    'Tre',
+    'Quattro',
+    'Cinque',
+    'Sei',
+    'Sette',
+    'Fante',
+    'Cavallo',
+    'Re'
+]
+
+let valori = [1,2,3,4,5,6,7,8,9,10]
+
+let colori =
+[
+    'Denari',
+    'Spade',
+    'Bastoni',
+    'Coppe'
+]
+
+class Carta
+{
+    nome;
+    valore;
+    colore;
+    
+    constructor(nome, valore, colore)
+    {
+        this.nome = nome;
+        this.valore = valore;
+        this.colore = colore;
+    }
+
+    toString()
+    {
+        return `${this.nome} di ${this.colore}`;
+    }
+}
+
+class Deck
+{
+    
+    listaCarte = [];
+
+
+    initialize()
+    {
+        colori.forEach(element => {
+            for (let i = 0; i<10; i++)
+            {
+                let carta = new Carta(nomi[i], valori[i], element);
+    
+                this.listaCarte.push(carta);
+            }
+        });
+    }
+
+    visualizzaDeck()
+    {
+        this.listaCarte.forEach(element => {
+            console.log(element.toString());
+        });
+    }
+
+    constructor()
+    {
+        let inizia = this.initialize;
+
+        let visualizza = this.visualizzaDeck;
+
+        this.initialize();
+
+        this.visualizzaDeck();
+    }
+
+    mescola = function ()
+    {
+        let counter = this.listaCarte.length - 1;
+        
+        while(counter > -1)
+        {
+            let indice = Math.floor(Math.random() * 9);
+    
+            let temp = this.listaCarte[counter];
+    
+            this.listaCarte[counter] = this.listaCarte[indice];
+    
+            this.listaCarte[indice] = temp;
+    
+            counter--;
+        }
+    
+    }
+}
+
+
+class Player
+{
+    nome;
+    punteggio;
+    mano;
+
+    constructor(nome)
+    {
+        this.nome = nome;
+
+        this.punteggio = 0;
+
+        this.mano = new Array();
+    }
+
+    toString()
+    {
+        return this.nome;
+    }
+}
+
+
+class Partita
+{
+    player1;
+
+    player2;
+
+    vincitore;
+
+    listaCarte;
+
+    r1;
+
+    constructor(player1, player2, listaCarte)
+    {
+        this.player1 = player1;
+
+        this.player2 = player2;
+
+        this.listaCarte = listaCarte;
+    }
+
+    chiVince()
+    {
+        if(this.player1.punteggio > this.player2.punteggio)
+            {this.vincitore = this.player1;}
+        else if(this.player1.punteggio == this.player2.punteggio)
+            {this.vincitore = undefined;}
+        else
+            {this.vincitore = this.player2;}
+
+        console.log(`The Winner is: ${this.vincitore}`);
+    }
+
+    gioca()
+    {
+        
+        r1.question('Vuoi una carta? \n s = sì, n=no \n', (answer) => {
+            
+            if(answer == "s")
+            {
+                this.player1.mano = this.listaCarte[0];
+    
+                console.log(this.player1.mano);
+            }
+
+            this.chiVince();
+          
+            r1.close();
+          });
+
+
+    }
+
+}
+
+
+let nuovoDeck = new Deck();
+
+
+for(let i = 0; i< 1000; i++)
+{
+    nuovoDeck.mescola();
+}
+
+
+let playerNuovo1 = new Player("Achille");
+
+let playerNuovo2 = new Player("Ettore");
+
+let partitaNuova = new Partita(playerNuovo1, playerNuovo2, nuovoDeck.listaCarte);
+
+partitaNuova.gioca();
